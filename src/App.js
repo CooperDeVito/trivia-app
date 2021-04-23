a8ff6950b1000b51314c664683a4fadc3c7a80b8
Cooper DeVito <ced3rq@virginia.edu>
2021-04-23 12:12:00 -0400

    distinct buttons, new theme

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 import React, { useState, useEffect } from "react";
 import Question from './Components/Question';
+import { Button, Typography, Grid, Paper } from '@material-ui/core';
+
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+
+
+
+const theme = createMuiTheme ({ 
+  palette: { 
+    type: 'light',
+
+    primary: { 
+      main: "#43A047"
+    },
+
+    secondary: {
+      main: "#0288D1"
+    }
+  }
+})
 
 function getRandomInt(min,max) { 
   min = Math.ceil(min);
@@ -7,10 +26,11 @@ function getRandomInt(min,max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+
 function App() {
   const [questions, setQuestions] = useState([]);
   useEffect(() => {
-    fetch("https://opentdb.com/api.php?amount=10&category=18&difficulty=hard&type=multiple")
+    fetch("https://opentdb.com/api.php?amount=10")
       .then((res) => res.json())
       .then((res) => {
         let tempQuestions = res.results; //temporarily hold the data 
@@ -23,31 +43,49 @@ function App() {
       });
   }, []);
 
+  /*
+  function showCorrect(questions) {
+    const newAnswers = questions.incorrect_answers.map((answer) => (
+      if (answer === questions.correct_answer) { 
+        
+      }
+    ))
+  }
+  */
   
-
-  
-
-
- 
   //make state variable or array of the questions 
   //an array that says if its the right answer
 
 
   return (
-    <div>
-      <h1> {getRandomInt(0,4)} </h1>
-      <h2>Trivia </h2>
+    <ThemeProvider theme={theme}>
+       <Paper> 
+       <div>
+      
+      <h2>Trivia App </h2>
       {questions.map((question) => (
-        <h1>{question.question}: 
-        <h5>
-        {question.incorrect_answers}
-        </h5>
-         </h1>
-
+        <h1>{question.question}:
+        <h1> {'\n'} </h1>
+        {question.incorrect_answers.map((answer) => (
+          <Button
+          color = 'secondary'
+          size = 'medium'
+          variant = 'contained'
+          type = 'button'
+          className = 'btn toggle-btn'> 
+          {answer} 
+          </Button>
+        ))}
+          
+        
+        </h1>
       ))}
 
       
     </div>
+      </Paper>
+      </ThemeProvider>
+    
   );
 }
 
