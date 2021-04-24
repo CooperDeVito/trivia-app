5cb5472e78a9342a2d1a7afe210ed1f5c385a64c
Cooper DeVito <ced3rq@virginia.edu>
2021-04-24 12:14:34 -0400

    install he

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -1,15 +1,73 @@
-import { React } from 'react';  
+import React, { useState, useEffect } from "react";
+import { Button, borders, Typography, Grid, Paper } from '@material-ui/core';
+import { MuiThemeProvider, ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import he from 'he';
 
 
+const theme = createMuiTheme ({ 
+    palette: { 
+        type: 'dark',
+
+      primary: { 
+        main: "#43A047" // a green
+      },
+  
+      secondary: {
+        main: "#e53935" // a red (standard)
+      },
+
+      success: { 
+          main: "#03A9F4" // a blue
+      }
+    }
+  })
 
 function Question(props) { 
 
+    const[isAnswered, setIsAnswered] = useState(false);
+
+    function handleClick () { 
+        setIsAnswered(true);
+    }
+
+    function correctAnswer (answer) { 
+        if (answer === props.question.correct_answer) { 
+            return 'primary';
+        }
+        else { 
+            return 'secondary';
+        }
+    }
+
     return ( 
-        <h1> 
+        <MuiThemeProvider theme={theme}>
+    <CssBaseline />
+            <Paper>
+        <h1>{props.question.question}:
+        <h1> {'\n'} </h1>
+        {props.question.incorrect_answers.map((answer) => (
+          <Button
+          color = {isAnswered ? correctAnswer(answer) : 'success' }
+          size = 'medium'
+          variant = 'contained'
+          type = 'button'
+          className = 'btn toggle-btn'
+          onClick = {handleClick} > 
+          
+          {answer} 
+          </Button>
+        ))}
+          
+        
+        </h1>
+            </Paper>
+        </MuiThemeProvider>
+        
 
             
 
-        </h1>
+        
     )
 
 }
